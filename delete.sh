aa45e5cc135b3eaa8defa792419ec8d39f6dec9d
이정화 <jhlee@ijeonghwaui-air.kr.ibm.com>
2019-09-26 14:17:51 +0900

    image version update to v0.1

--- delete.sh
+++ delete.sh
@@ -1,5 +1,9 @@
+kubectl delete deployment/cloudant-deployment-jaylee
+kubectl delete deployment/frontwebpage-jaylee
+kubectl delete deployment/getstartedjava-jaylee
 kubectl delete service/cloudant-service-jaylee
-kubectl delete service/fronwebpage-svc-jaylee
+kubectl delete service/frontwebpage-svc-jaylee
+kubectl delete service/getstartedjava-jaylee
 kubectl delete pvc/cloudant-pvc-jaylee
 kubectl delete pv/jaylee-pv
 
